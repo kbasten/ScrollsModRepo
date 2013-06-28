50a64717095205950f7961fc0f91a49530004c1a
Koen Basten <kbasten@gmail.com>
2013-06-28 02:27:15 +0200

    Further progress with submit page

--- cls/Submit.php
+++ cls/Submit.php
@@ -11,13 +11,22 @@
 			$this->loadMustache();
 			
 			$this->title = sprintf("Submit A Mod - %s Summoner Mod Repository", REPO_NAME);
-			$this->pageHeader = "Submit A Mod";
 			
-			$tpl = $this->m->loadTemplate("submit");
-			
-			$submitPage = $tpl->render(array(
-						"REPONAME" => REPO_NAME
-			));
+			if (isset($_POST["mod-name"])){
+				$this->pageHeader = "Thanks for your submission";
+				$tpl = $this->m->loadTemplate("submit_complete");
+				
+				// TODO: check actual parameters...
+				
+				$submitPage = $tpl->render();
+			} else {
+				$this->pageHeader = "Submit A Mod";
+				$tpl = $this->m->loadTemplate("submit");
+				
+				$submitPage = $tpl->render(array(
+							"REPONAME" => REPO_NAME
+				));
+			}
 			
 			$this->setResult(true, $this->getHTMLContent($submitPage));
 		}
